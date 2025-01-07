3e6c3e347f681977d3f116368f2f2b052e5391fb
Allowexe <106865010+Allowexe@users.noreply.github.com>
2025-01-07 13:02:00 +0100

    update

--- fr/isen/learning/interfaces/models/availabilityModel.java
+++ fr/isen/learning/interfaces/models/availabilityModel.java
@@ -2,10 +2,12 @@ package fr.isen.learning.interfaces.models;
 
 
 public class availabilityModel {
-    private String idFormation;
+    private String formationId;
 
     private int year;
 
     private char timeline;
+
+    private String availabilityId;
 
 }
